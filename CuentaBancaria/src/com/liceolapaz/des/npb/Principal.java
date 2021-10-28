8273aa7c1a66ab1ed5823ac93f5799332bf2f8c2
Nico <portelanicolas2001@gmail.com>
2021-10-28 10:06:47 +0200

    CuentaBancaria

--- CuentaBancaria/src/com/liceolapaz/des/npb/Principal.java
+++ CuentaBancaria/src/com/liceolapaz/des/npb/Principal.java
@@ -5,56 +5,143 @@ import java.util.Scanner;
 public class Principal {
 
 	public static void main(String[] args) {
-		String cuenta = null;
-		String tipoCuenta = null;
-		//Mostrar menú principal
-		menuPrincipal();
-		int opcion = elegirOpcion();
-		switch(opcion) {
-			default: {
-				System.out.println("No es una opción válida.");
-			}
-			case 1: {
-				menuSecundario();
-				opcion = elegirOpcion();
-				switch(opcion) {
-					default: {
-						System.out.println("No es una opción válida.");
-					}
-					case 1: {
-						System.out.print("Introduce el nombre de la cuenta: ");
-						cuenta = leerNombre();
-						tipoCuenta = "normal";
-					}
-					case 2: {
-						System.out.print("Introduce el nombre de la cuenta: ");
-						cuenta = leerNombre();
-						tipoCuenta = "premium";
-					}
-					case 0: {
-						System.exit(0);
-					}
-					
+		Integer cuenta = null;
+		String tipo_cuenta = null;
+		while (true) {
+			//Mostrar menú principal
+			menuPrincipal();
+			int opcion = elegirOpcion();
+			switch(opcion) {
+				case 0: {
+					System.exit(0);
 				}
+				//Opcion 1
+				//Mostrar submenú
+				case 1:
+					menuSecundario();
+					int opcion2 = elegirOpcion();
+					switch(opcion2) {
+						default: {
+							System.out.println("No es una opción válida.");
+							break;
+						}
+						//Pedir saldo inicial de la cuenta
+						//Cuenta normal permite tener saldo negativo, la premiun si
+						case 1: {	
+							cuenta = leerSaldo();
+							tipo_cuenta = "normal";
+							break;
+						}
+						case 2: {
+							cuenta = leerSaldo();
+							tipo_cuenta = "premium";
+							break;
+						}
+					}
+				break;
+
+				//Opcion 2
+				//Pedir cantidad para ingresar y comprobar que no es una cantidad negativa
+				case 2:
+					//Si no hay una cuenta activa hay que mandar a hacer una y no dejar ingresar dinero
+					if (cuenta == null) {
+						System.out.println("No hay una cuenta activa.");
+						break;
+					}
+					else {
+						int ingreso = 0;
+						while (ingreso <= 0) {
+							if (ingreso < 0) {
+								System.out.println("La cantidad no puede ser negativa.");
+								ingreso = leerCantidad();
+							}
+							else {
+								ingreso = leerCantidad();
+							}
+						}
+						cuenta = cuenta + ingreso;
+						System.out.println("El saldo de la cuenta es: " + cuenta);
+					}
+				break;
+			
+				//Opcion 3
+				//Pedir cantidad para retirar y comprobar que no es una cantidad negativa
+				case 3: 
+					if (cuenta == null) {
+						System.out.println("No hay una cuenta activa.");
+						break;
+					}
+					else {
+						if (tipo_cuenta == "premium") {
+							int retirada = leerCantidad();
+							if (retirada < 0) {
+								System.out.println("La cantidad no puede ser negativa.");
+								retirada = leerCantidad();
+							}
+							else {
+								cuenta = cuenta - retirada;
+								System.out.println("El saldo de la cuenta es: " + cuenta);
+							}
+						}
+						else if (tipo_cuenta == "normal") {
+							int cuenta_inicial = cuenta;
+							do {
+								int retirada = leerCantidad();
+								if (retirada < 0) {
+									System.out.println("La cantidad no puede ser negativa.");
+									retirada = 0;
+									cuenta = cuenta_inicial;
+									continue;
+								}
+								else {
+									cuenta = cuenta - retirada;
+									if (cuenta < 0) {
+										System.out.println("El saldo no puede ser negativo");
+										retirada = 0;
+										cuenta = cuenta_inicial;
+										continue;
+									}
+									//Al terminar de ingrsar o retirar dinero se mostrará el saldo
+									else {
+										System.out.println("El saldo de la cuenta es: " + cuenta);
+										break;
+									}
+								}
+							}
+							while(true);
+						}
+					}
+				break;
 				
-			}
+				case 4:
+					//Opcion 4
+					//Si no hay una cuenta activa mandará a crear una
+					if (cuenta == null) {
+						System.out.println("No hay una cuenta activa, crea una");
+						break;
+					}
+				//Mostrará el saldo de la cuenta
+				else {
+					System.out.println("EL saldo de la cuenta es: " + cuenta);
+				}
+			
+				default: 
+					System.out.println("No es una opción válida.");
+				break;
+			}	
 		}
-		//Opcion 1
-			//Mostrar submenú
-			//Pedir saldo inicial de la cuenta
-			//Cuenta normal permite tener saldo negativo, la premiun si
-		//Opcion 2 y 3
-			//Pedir cantidad para ingresar o retirar y comprobar que no es una cantidad negativa
-		//Si no hay una cuenta activa hay que mandar a hacer una y no dejar ingresar o retirar dinero
-		//Al terminar de ingrsar o retirar dinero se mostrará el saldo
-		//Opcion 4
-			//Mostrará el saldo de la cuenta
-			//Si no hay una cuenta activa mandará a crear una
 	}
 
-	private static String leerNombre() {
+	private static int leerCantidad() {
+		System.out.print("Escribe la cantidad: ");
 		Scanner escaner =  new Scanner(System.in);
-		return escaner.nextLine();
+		return escaner.nextInt();
+	}
+
+	private static int leerSaldo() {
+		System.out.print("Introduce el saldo inicial: ");
+		Scanner escaner =  new Scanner(System.in);
+		return escaner.nextInt();
 	}
 
 	private static int elegirOpcion() {
@@ -65,18 +152,18 @@ public class Principal {
 	}
 
 	private static void menuSecundario() {
-		System.out.println("Tipo de cuenta"
-				+ "1. Cuenta normal"
-				+ "2. Cuenta Premiun"
+		System.out.println("Tipo de cuenta \n"
+				+ "1. Cuenta normal \n"
+				+ "2. Cuenta Premiun \n"
 				+ "0. Cancelar");
 	}
 
 	private static void menuPrincipal() {
-		System.out.println("BANCO"
-				+ "1. Crear cuenta"
-				+ "2. Ingresar dinero"
-				+ "3. Retirar dinero"
-				+ "4. Consultar saldo"
+		System.out.println("BANCO \n"
+				+ "1. Crear cuenta \n"
+				+ "2. Ingresar dinero \n"
+				+ "3. Retirar dinero \n"
+				+ "4. Consultar saldo \n"
 				+ "0. Salir");
 		
 	}
